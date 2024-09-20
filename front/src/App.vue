81be39166d9cb278e39031c2e4dea871bc174896
Quanghng <90704201+Quanghng@users.noreply.github.com>
2024-09-20 10:19:31 +0200

    added checkAuth

--- front/src/App.vue
+++ front/src/App.vue
@@ -1,6 +1,11 @@
 <script setup>
 import Navbar from "./components/Navbar.vue";
 import { RouterView } from "vue-router";
+import { useAuthStore } from "./stores/AuthStore";
+
+const authStore = useAuthStore();
+// Call checkAuth on app load
+authStore.checkAuth();
 </script>
 
 <template>
